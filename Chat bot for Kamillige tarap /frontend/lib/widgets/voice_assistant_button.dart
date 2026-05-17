a1cf84139921826591a5eb6a51fceb70b51f3796
Jasmin08Coder93ML <d63575500@gmail.com>
2026-05-17 20:21:05 +0500

    Delete Chat bot for Kamillige tarap  directory

--- Chat bot for Kamillige tarap /frontend/lib/widgets/voice_assistant_button.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:your_project_name/widgets/voice_assistant_button.dart'; // Täze döreden faýlyňy import et
-
-// ... Sahypanyň kody ...
-Scaffold(
-  body: Center(child: Text("Jasmine Kömekçisi")),
-  floatingActionButton: VoiceAssistantButton(), // Düwmäni şu ýere goş
-)
